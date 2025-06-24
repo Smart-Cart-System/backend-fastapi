c8c5e5376fd057cef11453e465c81cadc4fb47e9
Zafaranii <marwan.elzafarani@gmail.com>
2025-06-24 12:14:59 +0300

    update_user_info

--- routers/auth.py
+++ routers/auth.py
@@ -3,9 +3,10 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from database import get_db
-from core.security import create_frontend_token, verify_password, check_admin_permissions
+from core.security import create_frontend_token, verify_password, check_admin_permissions, get_current_user
+from models.user import User
 from crud import user as user_crud
-from schemas.user import UserOut, UserCreate
+from schemas.user import UserOut, UserCreate, UserBase, PasswordUpdateForm
 from services.logging_service import LoggingService, SecurityEventType, get_logging_service
 from core.config import settings
 from typing import Optional
@@ -99,3 +100,35 @@ def login(request: Request,
         "is_admin": user.is_admin,
         "sql_string": settings.SQLALCHEMY_DATABASE_URL if user.is_admin else None
     }
+
+@router.put("/update-data", response_model=UserOut)
+def update_user_data(
+    user: UserBase, 
+    db: Session = Depends(get_db), 
+    current_user: User = Depends(get_current_user)
+):
+    db_user = user_crud.get_user_by_username(db, username=current_user.username)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    updated_user = user_crud.update_user(db=db, user_id=db_user.id, user_data=user)
+    
+    
+    return updated_user
+
+@router.put("/update-password")
+def update_password(
+    request: Request,
+    form_data: PasswordUpdateForm,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+
+):
+    user = user_crud.get_user_by_username(db, username=form_data.username)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    
+    user_crud.update_user_password(db=db, user_id=user.id, new_password=form_data.new_password)
+    
+    return {"detail": "Password updated successfully"}
+
